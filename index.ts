90125454026bf3dd0236af3b6f527bb654881165
Josiah Sayers <josiah.sayers15@gmail.com>
2023-12-02 19:49:00 -0500

    add file content to exercise day creation

--- index.ts
+++ index.ts
@@ -41,17 +41,56 @@ async function getInput() {
 }
 
 async function createExerciseDay(day: number) {
-  const requiredFiles = [
+  const emptyFiles = [
     'input.txt',
     'problem.md',
-    'solution.ts',
   ];
   
   await mkdir(`2023/${day}`, { recursive: true });
 
-  for (const requiredFile of requiredFiles) {
-    const file = Bun.file(`2023/${day}/${requiredFile}`);
+  for (const emptyFile of emptyFiles) {
+    const file = Bun.file(`2023/${day}/${emptyFile}`);
     await Bun.write(file, '');
-    console.log(`Created file: 2023/${day}/${requiredFile}`);
+    console.log(`Created file: 2023/${day}/${emptyFile}`);
   }
+
+  const solutionFile = Bun.file(`2023/${day}/solution.ts`);
+  const testFile = Bun.file(`2023/${day}/solution.test.ts`);
+
+  await Bun.write(solutionFile, 
+`const showWork = Bun.env.SHOW_WORK === 'true';
+const file = Bun.file('2023/${day}/input.txt');
+const input = await file.text();
+const part1Answer = part1(input);
+const part2Answer = part2(input);
+console.log('Part 1: ' + part1Answer);
+console.log('Part 2: ' + part2Answer);
+
+export function part1(input) {
+  return null;
+}
+
+export function part2(input) {
+  return null;
+}
+`);
+console.log(`Created file: 2023/${day}/solution.ts`);
+
+await Bun.write(testFile,
+`import { describe, expect, test } from "bun:test";
+import { part1, part2 } from "./solution";
+
+describe('Day ${day}', () => {
+  const exampleInput = null;
+
+  test('returns the correct output for part 1', () => {
+    expect(part1(exampleInput)).not.toBeNull();
+  });
+
+  test('returns the correct output for part 2', () => {
+    expect(part2(exampleInput)).not.toBeNull();
+  });
+});
+`)
+console.log(`Created file: 2023/${day}/solution.test.ts`);
 }
